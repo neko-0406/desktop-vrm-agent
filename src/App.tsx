bc6804ae5796a0e697570ae293fc9f6dc592d7e3
momiji <sateraito3696@gmail.com>
2026-05-10 21:47:55 +0900

    テスト用データを除外、VRMモデルを読み込み→表示まで実行

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { VrmViewer } from "./components/VrmViewer";
 
 function App() {
-  return <div id="root"></div>;
+  const [vrmUrl, setVrmUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    setVrmUrl("/avatar.vrm");
+  }, []);
+
+  return (
+    <main className="app-main">
+      <VrmViewer vrmUrl={vrmUrl} />
+    </main>
+  );
 }
 
 export default App;
